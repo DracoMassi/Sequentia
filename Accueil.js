53bebaa4d2ab08c5325fcf4c58b2f4076648a9f8
DracoMassi <massinissabouhadouf@gmail.com>
2025-08-08 23:51:58 +0200

    Update Accueil.js

--- Accueil.js
+++ Accueil.js
@@ -179,16 +179,3 @@ if (menu && menuGrand) {
     menuGrand.classList.toggle('clicked');
   });
 }
-function ajusterLargeurBody() {
-  // Récupérer la largeur de la fenêtre
-  const largeurFenetre = window.innerWidth;
-
-  // Appliquer cette largeur au body
-  document.body.style.width = largeurFenetre + "px";
-}
-
-// Ajuster au chargement de la page
-window.addEventListener("load", ajusterLargeurBody);
-
-// Ajuster aussi à chaque redimensionnement
-window.addEventListener("resize", ajusterLargeurBody);
